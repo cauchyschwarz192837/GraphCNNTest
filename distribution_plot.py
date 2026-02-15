d4214c2172af8f27ce670cb1bf799ca61106ae9e
andre <107526107+cauchyschwarz192837@users.noreply.github.com>
2026-02-14 19:51:12 -0500

    Update distribution_plot.py

--- distribution_plot.py
+++ distribution_plot.py
@@ -10,8 +10,8 @@ datadf = pd.read_csv('distribution_plot.csv', delimiter=',')
 datadf = datadf.to_numpy()
 
 plt.hist(datadf, bins=np.arange(min(datadf), max(datadf)+0.48, step=0.48))
-plt.title('sdfsdfnjsdfoskdmoskdfmsjdnfsdfsdf')
-plt.xlabel("sdfsdfsdf)")
-plt.ylabel("sdfsdfsdfsdf")
+plt.title('Distribution plot of collated piezoelectric coefficients')
+plt.xlabel("d33")
+plt.ylabel("Count")
 
 plt.show()
